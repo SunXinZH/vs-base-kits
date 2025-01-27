0348d9ccbc5787f3bf19d73694e4e0d91942b936
SunXin <xin.sun.zh@gmail.com>
2025-01-27 15:54:53 +0800

    add throttler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Event, Emitter } from './common/event';
 export { IDisposable, Disposable, DisposableStore } from './common/lifecycle';
 export { Barrier } from './common/barrier';
+export { Throttler } from './common/throttler';
+
